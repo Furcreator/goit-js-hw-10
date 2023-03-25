3d57e52a6a01ac4a383f88caa7bb3c8e2e4b0b0d
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-03-25 13:00:34 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchInput = document.querySelector('#search-box');
+const list = document.querySelector('#country-list');
+const info = document.querySelector('#country-info');
+
+searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(searchInput) {
+    let inputText = searchInput.target.value.trim();
+    console.log(inputText);
+    fetchCountries(inputText)
+}
