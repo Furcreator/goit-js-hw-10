4b2a7aefb565bb79d86ab0bfee95d8aaab471a37
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-03-26 19:09:37 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
 const searchInput = document.querySelector('#search-box');
-const list = document.querySelector('#country-list');
-const info = document.querySelector('#country-info');
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
 
 searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
@@ -56,23 +56,25 @@ function showInfoMessage() {
     Notify.info("Too many matches found. Please enter a more specific name.", {timeout: 1200});
 }
 
-function showCountryInfo(country) {
-
-    let countryInfo = `<img class="i-flag" src="" alt="" />
-    <h2 class="i-title"></h2>
-    <p><strong>Capital:</strong></p>
-    <p><strong>Population:</strong></p>
-    <p><strong>Languages:</strong></p>`
+function showCountryInfo({ name: { official }, capital, population, flags: { png, alt }, languages }) {
+    let language = Object.values(languages)
+    let countryInfo = `<div class="container">
+    <img class="i-flag" src="${png}" alt="${alt}" />
+    <h2 class="i-title">${official}</h2>
+    </div>
+    <p><strong>Capital: </strong>${capital}</p>
+    <p><strong>Population: </strong>${population}</p>
+    <p><strong>Languages: </strong>${language}</p>`
     info.innerHTML = countryInfo;
 }
 
 function showCountriesList(countries) {
     console.log(countries);
-
     let listItems = countries.map(country => {
-        return `<li>
-        <img class="l-flag" src="" alt="" />
-        <h2 class="l-title"></h2>
+        const { name: { official }, flags: { png, alt } } = country;
+        return `<li class = "list">
+        <img class="l-flag" src="${png}" alt="${alt}" />
+        <h2 class="l-title">${official}</h2>
                 </li>`
     }).join('')
 
